a9b4a4ee5ac6d49021347f69ae39be8c522d2a03
Daniel Gavidia <dgavidia1@gmail.com>
2024-10-12 13:14:34 -0400

    Finished implementing logic scraping movie data, with descriptions from individual movie sites

--- index.ts
+++ index.ts
@@ -1,9 +1,16 @@
-const puppeteer = require("puppeteer");
+import puppeteer from "puppeteer";
 const UserAgent = require("user-agents");
 
 const url = "https://www.imdb.com/chart/top/";
 
-async function main() {
+type Movie = {
+	title: string;
+	img: string;
+	imdbLink: string;
+	description?: string;
+};
+
+const moviesMain = async (): Promise<Movie[]> => {
 	// Start a browser instance
 	const browser = await puppeteer.launch({
 		headless: true,
@@ -19,12 +26,49 @@ async function main() {
 	// Take page to url
 	await page.goto(url);
 
-	// Console log HTML content of the site
-	console.log(await page.content());
+	// Get movie data
+	const movieData: Movie[] = await page.evaluate(() => {
+		const movies = Array.from(document.querySelectorAll(".ipc-metadata-list-summary-item"));
+		const moviesParsed = movies.map((movie) => {
+			const title = movie.querySelector(".ipc-title__text")?.textContent?.trim();
+			const img = movie.querySelector(".ipc-image")?.getAttribute("src");
+			const imdbLink = movie.querySelector(".ipc-title-link-wrapper")?.getAttribute("href");
+			return {
+				title: title ? title : "",
+				img: img ? img : "",
+				imdbLink: imdbLink ? `https://www.imdb.com${imdbLink}` : "",
+			};
+		});
+		return moviesParsed;
+	});
+
+	// Get movie description from individual movie pages
+	const moviesParsed: Movie[] = await Promise.all(
+		movieData.slice(0, 10).map(async (movie) => {
+			// Navigate to movie page
+			if (movie.imdbLink === "") {
+				return { ...movie, description: "" };
+			}
+			const moviePage = await browser.newPage();
+			await moviePage.setUserAgent(userAgent);
+			await moviePage.goto(movie.imdbLink);
+
+			// Get description
+			const description = await moviePage.evaluate(() => {
+				return document.querySelector(".sc-55855a9b-0")?.textContent?.trim();
+			});
+			// console.log(description);
+			await moviePage.close();
+			return { ...movie, description: description ? description : "" };
+		})
+	);
 
 	// Close browser and tab
 	await page.close();
 	await browser.close();
-}
 
-main();
+	// Return data;
+	return moviesParsed;
+};
+
+await moviesMain();
